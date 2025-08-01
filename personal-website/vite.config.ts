74be25b5227bbc88fd0d77886fec44509947e319
KikaOrdaz <45240183+KikaOrdaz@users.noreply.github.com>
2025-08-01 02:08:05 +0100

    bug

--- personal-website/vite.config.ts
+++ personal-website/vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base:  "/PresentationWebsite/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: './',
+  plugins: [react()],
 })
